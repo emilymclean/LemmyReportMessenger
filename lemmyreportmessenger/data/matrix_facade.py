d746b387a922c92a4d3b806f7c9b922471152c7c
Ben McLean <benmclean007@gmail.com>
2024-02-12 11:45:38 +1100

    Setup initialisation code

--- lemmyreportmessenger/data/matrix_facade.py
+++ lemmyreportmessenger/data/matrix_facade.py
@@ -8,21 +8,22 @@ from lemmyreportmessenger.data import ContentType
 class MatrixFacade:
     client: AsyncClient
     room_id: str
-    instance_url: str
+    lemmy_instance: str
 
     def __init__(self, client: AsyncClient, room_id: str, instance_url: str):
         self.client = client
         self.room_id = room_id
-        self.instance_url = instance_url
-        asyncio.run(self.join_room())
+        self.lemmy_instance = instance_url
+        asyncio.run(self._join_room())
 
-    async def join_room(self):
-        if self.room_id not in (await self.client.joined_rooms()).rooms:
-            await self.client.join(self.room_id)
+    async def _join_room(self):
+        if self.room_id in (await self.client.joined_rooms()).rooms:
+            return
+        await self.client.join(self.room_id)
 
     async def send_report_message(self, content_id: int, content_type: ContentType, reason: str):
-        await self.join_room()
-        url = f"{self.instance_url}/{'post' if content_type == ContentType.POST else 'comment'}/{content_id}"
+        await self._join_room()
+        url = f"{self.lemmy_instance}/{'post' if content_type == ContentType.POST else 'comment'}/{content_id}"
 
         await self.client.room_send(
             room_id=self.room_id,
--- lemmyreportmessenger/lemmyreportmessenger.py
+++ lemmyreportmessenger/lemmyreportmessenger.py
@@ -3,6 +3,10 @@ import traceback
 from time import sleep
 from typing import List
 
+from nio import AsyncClient
+from plemmy import LemmyHttp
+from plemmy.responses import GetCommunityResponse
+
 from lemmyreportmessenger.data import ReportPersistence, LemmyFacade, Report
 from lemmyreportmessenger.data.matrix_facade import MatrixFacade
 from lemmyreportmessenger.reconnection_manager import ReconnectionDelayManager
@@ -10,20 +14,67 @@ from lemmyreportmessenger.reconnection_manager import ReconnectionDelayManager
 
 class LemmyReportMessenger:
     community_ids: List[int]
-    reconnection_manager: ReconnectionDelayManager
+    reconnection_manager: ReconnectionDelayManager = ReconnectionDelayManager()
     report_persistence: ReportPersistence
     lemmy_facade: LemmyFacade
     matrix_facade: MatrixFacade
+
+    def __init__(
+            self,
+            community_ids: List[int],
+            report_persistence: ReportPersistence,
+            lemmy_facade: LemmyFacade,
+            matrix_facade: MatrixFacade
+    ):
+        self.community_ids = community_ids
+        self.report_persistence = report_persistence
+        self.lemmy_facade = lemmy_facade
+        self.matrix_facade = matrix_facade
+
+    @staticmethod
+    def create(
+            community_names: List[str],
+            lemmy_instance: str,
+            lemmy_username: str,
+            lemmy_password: str,
+            matrix_instance: str,
+            matrix_username: str,
+            matrix_password: str,
+            matrix_room: str
+    ):
+        lemmy_http = LemmyHttp(lemmy_instance)
+        lemmy_http.login(lemmy_username, lemmy_password)
+
+        community_ids = [GetCommunityResponse(
+            lemmy_http.get_community(name=c)
+        ).community_view.community.id for c in community_names]
+
+        client = AsyncClient(matrix_instance)
+        client.login(matrix_username, matrix_password)
+
+        return LemmyReportMessenger(
+            community_ids,
+            ReportPersistence(),
+            LemmyFacade(
+                lemmy_http
+            ),
+            MatrixFacade(
+                client,
+                matrix_room,
+                lemmy_instance
+            )
+        )
 
     def run(self):
         while True:
             # noinspection PyBroadException
             try:
                 self.scan()
-                sleep(60)
             except Exception:
                 print(traceback.format_exc())
                 self.reconnection_manager.wait()
+
+            sleep(60)
 
     def scan(self):
         for community_id in self.community_ids:
